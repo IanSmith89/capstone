e4e6a43f952ee66a2996af5551368c3b1cef2c64
Ian Smith <iansmith1026@gmail.com>
2016-03-28 15:46:19 -0600

    reworked menu bar

--- salvageApp/app/scripts/directives.js
+++ salvageApp/app/scripts/directives.js
@@ -8,11 +8,11 @@ function comparePassword() {
     require: 'ngModel',
     link: function(scope, elem, attrs, ctrl) {
       ctrl.$parsers.unshift(function(viewValue, $scope) {
-        var match = viewValue === scope.registration.password.$viewValue;
-        if (match) {
-          ctrl.$setValidity('match', true);
-        } else {
+        var noMatch = viewValue !== scope.registration.password.$viewValue;
+        if (noMatch) {
           ctrl.$setValidity('match', false);
+        } else {
+          ctrl.$setValidity('match', true);
         }
         return viewValue;
       });
